b4d2e7f8adc871d9203363d5186b66cea86449d5
kapilesh-ap <151802457+kapilesh-ap@users.noreply.github.com>
2023-12-12 16:54:18 -0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -5,9 +5,43 @@ import tensorflow as tf
 import streamlit as st
 import imageio
 import os
-from utils import load_data,num_to_char
-from modelutil import load_model
+
 import numpy as np
+
+import os 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+
+def load_model() -> Sequential: 
+    model = Sequential()
+
+    model.add(Conv3D(128, 3, input_shape=(75,46,140,1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(256, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(Conv3D(75, 3, padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPool3D((1,2,2)))
+
+    model.add(TimeDistributed(Flatten()))
+
+    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Bidirectional(LSTM(128, kernel_initializer='Orthogonal', return_sequences=True)))
+    model.add(Dropout(.5))
+
+    model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
+
+    model.load_weights(os.path.join('model', 'checkpoint'))
+
+    return model
+
+
 
 #setting the layout of streamlit as wide
 st.set_page_config(layout='wide')
